efcb873443bbba054947a6ffb03763bc9f116d88
Sarthak Mishra <sarthakmishra1605@gmail.com>
2024-09-16 15:10:09 +0530

    Updated JS File

--- JS.js
+++ JS.js
@@ -1,13 +1,16 @@
 let input=document.querySelector(".input");
-
 let ans=document.querySelector(".answer");
 
+
+//Calculate Button
 let btn=document.querySelector(".calculate");
 btn.addEventListener("click", () => {
     let answer=eval(input.value);
     ans.innerText=answer;
 })
 
+
+// Clear Button
 let btn2=document.querySelector(".clear");
 btn2.addEventListener("click", () =>{
     input.value="";
@@ -16,22 +19,25 @@ btn2.addEventListener("click", () =>{
 })
 
 
+// Input through Button
 let num=document.querySelectorAll(".number, .operator");
-
 num.forEach((x) => {
     x.addEventListener("click", () => {
     input.value+=x.innerText;
     });
 });
 
+
+// Enter and Delete through KeyBoard
 input.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
+    if(event.key==="Enter"){
         let answer=eval(input.value);
         ans.innerText=answer;
     }
 });
 
 
+// "X" 
 let del=document.querySelector(".delete");
 del.addEventListener("click", () => {
     input.value=input.value.slice(0, -1);
